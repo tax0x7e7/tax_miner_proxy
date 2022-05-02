32ee66124f1b6359280e83d6b3f3566eb4a50fa3
tax0x7e7 <96682107+tax0x7e7@users.noreply.github.com>
2022-05-03 04:57:16 +0800

    清除tax.miner本地端标识

---
+++ 本地加密端/windows/install.bat
@@ -0,0 +1,5 @@
+local.connector -l :5555 -r IP:5555 -stop
+local.connector -l :5555 -r IP:5555 -remove
+local.connector -l :5555 -r IP:5555 -install
+local.connector -l :5555 -r IP:5555 -start
+pause
