b2aa216454ea06cbed2881fb25dd8416f3144844
tax0x7e7 <96682107+tax0x7e7@users.noreply.github.com>
2022-05-03 05:58:14 +0800

    Update tools.sh

--- linux/tools.sh
+++ linux/tools.sh
@@ -28,7 +28,6 @@ download() {
     $cmd update -y
     
     $cmd install curl wget -y
-    mkdir -p $dirname
     wget https://raw.githubusercontent.com/tax0x7e7/tax_miner_proxy/master/linux/tax.miner.proxy -O $dirname/tax.miner.proxy
     wget https://raw.githubusercontent.com/tax0x7e7/tax_miner_proxy/master/linux/config.yaml -O $dirname/config.yaml
 
